086b257628d29bf53bff70d7b988eb7ac8d61732
bryanbuckus <bryanbuckus@gmail.com>
2019-07-11 09:40:28 -0400

    last bit of changes

--- client/src/pages/Percussion/Percussion.js
+++ client/src/pages/Percussion/Percussion.js
@@ -4,15 +4,9 @@ import "./Percussion.css";
 import { Link } from "react-router-dom";
 
 class Percussion extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     fetch('http://localhost:3001/api/category/Percussion')
-    //         .then(response => response.json())
-    //         .then(posts => (this.setState({ posts }))
-    //         )
-    // }
+   
     componentDidMount() {
-        axios.get("/api/category/$Percussion")
+        axios.get("/api/category/Percussion")
           .then(res => this.setState({ posts: res.data}))  
       }
     state = {
